7e0144f326b54973ace1f11dd6e3675aec73bfe6
Aroweeri <dylanlsutton@gmail.com>
2017-07-09 11:58:51 -0400

    Checkpoint.

--- boogie_board.c
+++ boogie_board.c
@@ -6,12 +6,15 @@
 #define PRESS 33
 #define SIGNAL 27
 
-void capture(FILE *f, unsigned char *s, int bytes);
+void capture(FILE *f, unsigned char *s, int bytes, short *vertical, short *horizontal);
+short combine(char dollars, char cents);
 
 int main() {
 	int i;
 	int counter = 0;
 	int zero=1;
+	short vertical;
+	short horizontal;
 	int bytes=8;
 	unsigned char s[bytes];
 	FILE *f = fopen("/dev/usb/hiddev0", "rb");
@@ -20,17 +23,19 @@ int main() {
 		fread(s, sizeof(char), bytes, f);
 		
 		if(s[4] == 27) {
-			capture(f, s, bytes);
+			capture(f, s, bytes, &vertical, &horizontal);
 		}
 	}
 
 	return 0;
 }
 
-void capture(FILE *f, unsigned char *s, int bytes) {
+void capture(FILE *f, unsigned char *s, int bytes, short *vertical, short *horizontal) {
 
 	int i;
 	int hover=0;
+	char cents;
+	char dollars;
 
 	//eat up two lines of useless information
 	fread(s, sizeof(char), bytes, f);
@@ -43,24 +48,45 @@ void capture(FILE *f, unsigned char *s, int bytes) {
 	} else if (s[4] == PRESS) {
 		hover=0;	
 	}
+
+	fread(s, sizeof(char), bytes, f);
+	cents = s[4];
 	
+	fread(s, sizeof(char), bytes, f);
+	dollars = s[4];
 	
-	fflush(stdout);
+	printf("%d%d\n", dollars, cents);
+	//*vertical = combine(cents, dollars);
+
+	fread(s, sizeof(char), bytes, f);
+	cents = s[4];
+	
+	fread(s, sizeof(char), bytes, f);
+	dollars = s[4];
+	
+	printf("%0x%0x\n", dollars, cents);
+	//*horizontal = combine(cents, dollars);
+
+}
+
+short combine(char dollars, char cents) {
+	short combined = 0;
+	return combined;
 }
 
 
-//00 00 06 00 1b 00 00 00 
-//00 00 06 00 0e 00 00 00 
-//00 00 06 00 01 00 00 00 
-//00 00 06 00 20 00 00 00 
-//00 00 06 00 58 00 00 00 
-//00 00 06 00 07 00 00 00 
-//00 00 06 00 a8 00 00 00 
-//00 00 06 00 15 00 00 00 
-//00 00 06 00 00 00 00 00 
-//00 00 06 00 00 00 00 00 
-//00 00 06 00 20 00 00 00 
-//00 00 06 00 00 00 00 00 
-//00 00 06 00 00 00 00 00 
-//00 00 06 00 00 00 00 00 
-//00 00 06 00 00 00 00 00
+//00 00 06 00 1b 00 00 00 start
+//00 00 06 00 0e 00 00 00 //
+//00 00 06 00 01 00 00 00 //
+//00 00 06 00 20 00 00 00 press or hover
+//00 00 06 00 58 00 00 00 vertical cents
+//00 00 06 00 07 00 00 00 vertical dollars
+//00 00 06 00 a8 00 00 00 horizontal cents
+//00 00 06 00 15 00 00 00 horizontal dollars
+//00 00 06 00 00 00 00 00 //
+//00 00 06 00 00 00 00 00 //
+//00 00 06 00 20 00 00 00 //
+//00 00 06 00 00 00 00 00 //
+//00 00 06 00 00 00 00 00 //
+//00 00 06 00 00 00 00 00 //
+//00 00 06 00 00 00 00 00 //
